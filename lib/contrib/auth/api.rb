45c3e0d9c8c053669dea5e12f83344d751ba9516
Nando Sousa <nandosousafr@gmail.com>
2021-10-28 09:56:35 -0300

    Add sign_up_with_email_and_password on the interface

--- lib/contrib/auth/api.rb
+++ lib/contrib/auth/api.rb
@@ -13,6 +13,10 @@ module Contrib
       def reset_password(email_or_username)
         @provider.reset_password(email_or_username)
       end
+
+      def sign_up_with_email_and_password(email_or_username, password)
+        @provider.sign_up_with_email_and_password(email_or_username, password)
+      end
     end
   end
 end
